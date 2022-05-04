b2fd7f577d11013bea69dd39ccca9b688bad809b
cynthiabillisich <101584671+cynthiabillisich@users.noreply.github.com>
2022-05-04 17:59:42 +0200

    showing the current temperature and conditions

---
+++ src/app.js
@@ -0,0 +1,16 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = Math.round(response.data.name);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "3856c9c7e9e248120caed23d08f8cd4b";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
