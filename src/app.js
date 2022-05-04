022655f31a0076ba2cb4198c9945f1eebfea8b0d
cynthiabillisich <101584671+cynthiabillisich@users.noreply.github.com>
2022-05-04 18:27:25 +0200

    corrected city element

--- src/app.js
+++ src/app.js
@@ -29,13 +29,14 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = Math.round(response.data.name);
+  cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "3856c9c7e9e248120caed23d08f8cd4b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Vienna";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
