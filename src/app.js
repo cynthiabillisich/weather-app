1bdd671a8bd4b6649f2dca3f3774641ce2c66b94
cynthiabillisich <101584671+cynthiabillisich@users.noreply.github.com>
2022-05-06 13:58:39 +0200

    added search engine

--- src/app.js
+++ src/app.js
@@ -42,8 +42,18 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "3856c9c7e9e248120caed23d08f8cd4b";
-let city = "Vienna";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "3856c9c7e9e248120caed23d08f8cd4b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+search("Vienna");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
